19a3692b6389880f852aa366cb620282da317fb4
Édua <edua.kaszas@gmail.com>
2022-06-04 21:55:14 +0200

    ESLint and Prettier configurations

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
-import { render } from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 import Root from './components/Root';
 
-const rootElement = document.getElementById('root');
-render(<Root />, rootElement);
+const rootElement = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+
+rootElement.render(<Root />);
