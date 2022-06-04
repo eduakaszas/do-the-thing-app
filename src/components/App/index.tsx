94068f1aa44fba370effb68faf3e8600f3402151
Édua <edua.kaszas@gmail.com>
2022-06-04 19:11:14 +0200

    Set up initial configuration of the app

---
+++ src/components/App/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { useStyles } from './useStyles';
+
+export default function App() {
+  const css = useStyles;
+
+  return <div className={css.container}>Hello world</div>;
+}
